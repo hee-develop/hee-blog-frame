775ec590e33a6ed57a115d799597243e8f608236
hee-develop <hee950320@yahoo.co.jp>
2021-05-05 15:52:43 +0900

    add Header and footer in Layout

--- components/HtmlHead.tsx
+++ components/HtmlHead.tsx
@@ -1,6 +1,6 @@
 import NextHead from 'next/head';
 
-export function HtmlHead({ title, meta }) {
+export default function HtmlHead({ title, meta }) {
   const headTitle = title || 'hee dev blog';
 
   return (
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,27 @@
-import Head from "./Head";
+import { ReactChild } from 'react';
+import HtmlHead from './HtmlHead';
+import Header from './Header';
+import styled from 'styled-components';
 
-export function Layout({ children, meta }) {
+type LayoutProps = {
+  children: ReactChild | JSX.Element[];
+  meta?: object;
+}
+
+const Main = styled.main`
+  padding-top: 80px;
+`;
+
+export function Layout({ children, meta } : LayoutProps) {
   return (
     <>
-      <Head title={''} meta={{}} />
+      <HtmlHead title={''} meta={{}} />
 
-      <main>{children}</main>
+      <Header />
+      <Main>{children}</Main>
+      <footer>
+        2021 hee-develop
+      </footer>
     </>
   );
 }
