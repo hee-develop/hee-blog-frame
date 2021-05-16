7af814c54a32045dca9a89c5ca2fbd9181fc4dbb
hee-develop <hee950320@yahoo.co.jp>
2021-05-16 11:16:50 +0900

    add typescript plugin

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteMetadata: {
+    title: "hee dev blog",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "-",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
