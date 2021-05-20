9daec7d07b2ed026a795fb0696bd25cc3277e3cb
hee-develop <hee950320@yahoo.co.jp>
2021-05-20 20:19:36 +0900

    add index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,68 @@
+import { graphql } from 'gatsby';
+import React from 'react';
+import Layout from '../components/Layout'
+import PostCard from '../components/index/PostCard';
+
+type IndexData = {
+  site: {
+    siteMetadata: {
+      title: string
+    }
+  }
+
+  allMarkdownRemark: {
+    nodes: Array<{
+      // excerpt
+      fields: {
+        slug: string
+      }
+      frontmatter: {
+        date: string
+        title: string
+        // descrpition
+      }
+    }>
+  }
+}
+
+export default function Index({ data }: { data: IndexData }) {
+  const siteTitle = data.site.siteMetadata?.title ?? 'hee dev blog';
+  const posts = data.allMarkdownRemark.nodes;
+
+  return (
+    <Layout pageTitle={siteTitle}>
+      {posts.map(post => (
+        <PostCard
+          path={post.fields.slug}
+          title={post.frontmatter.title}
+          writtenIn={post.frontmatter.date}
+        />
+      ))}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    # site metadata
+    site {
+      siteMetadata {
+        title
+      }
+    }
+
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          # description
+        }
+      }
+    }
+  }
+`;
